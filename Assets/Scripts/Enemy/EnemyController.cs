22c85d42502ea0f66fcbf48202df110960d234f7
TunnTapCode <hoangtuan69a1@gmail.com>
2024-02-22 22:43:49 +0700

    code boss can chien

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform target;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector2(0.2604211f, 0.2491516f);
+        }
+        else
+        {
+            transform.localScale = new Vector2(-1* 0.2604211f, 0.2491516f);
+
+        }
+       
+       
+    }
+}
